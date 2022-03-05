184030a8fd5c813d924dfefc3be7fe229108f3c8
Kangooboy <40115049+Kangooboy@users.noreply.github.com>
2022-03-05 19:38:55 +0200

    function to create objects

--- js/main.js
+++ js/main.js
@@ -1,16 +1,3 @@
-const getRandomInt = function (first, second) {
-  const min = (first < second) ? Math.ceil(first) : Math.floor(second);
-  const max = (first > second) ? Math.ceil(first) : Math.floor(second);
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-};
-
-/*
-function verifyMaxLength (string, maxLength) {
-  return string.length > maxLength;
-}
-verifyMaxLength('qwerty', 10);
-*/
-
 const MIN_AVATAR_COUNT = 1;
 const MAX_AVATAR_COUNT = 6;
 const MIN_LIKES_COUNT = 15;
@@ -18,15 +5,10 @@ const MAX_LIKES_COUNT = 200;
 const PHOTOS_COUNT = 25;
 const COMMENT_MESSAGES = [
   'Всё отлично!',
-
   'В целом всё неплохо. Но не всё.',
-
   'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
-
   'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
-
   'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
-
   'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
 ];
 const DESCRIPTIONS = [
@@ -40,6 +22,12 @@ const DESCRIPTIONS = [
 ];
 const AUTHOR_NAMES = ['Alex', 'Sergey', 'Max', 'Dima', 'Anna', 'Inna'];
 
+const getRandomInt = function (first, second) {
+  const min = (first < second) ? Math.ceil(first) : Math.floor(second);
+  const max = (first > second) ? Math.ceil(first) : Math.floor(second);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
 const getAvatarUrl = () => {
   const avatarId = getRandomInt(MIN_AVATAR_COUNT, MAX_AVATAR_COUNT);
   return `img/avatar-${avatarId}.jpg`;
@@ -47,24 +35,23 @@ const getAvatarUrl = () => {
 
 const getRandomArrayElement = (elements) => elements[getRandomInt(0, elements.length - 1)];
 
-const getCommentsData = (id) => ({
+const getCommentData = (id) => ({
   id,
   avatar: getAvatarUrl(),
   message: getRandomArrayElement(COMMENT_MESSAGES),
   name: getRandomArrayElement(AUTHOR_NAMES),
 });
 
-const getCommentData = () => Array.from({ length: 2 }, (v, k) => getCommentsData(k + 1));
+const getCommentsData = () => Array.from({ length: 2 }, (v, k) => getCommentData(k + 1));
 
-const getPicturePhoto = (id) => ({
+const getPhotoDescription = (id) => ({
   id,
   url: `photos/${id}.jpg`,
   description: getRandomArrayElement(DESCRIPTIONS),
   likes: getRandomInt(MIN_LIKES_COUNT, MAX_LIKES_COUNT),
-  comments: getCommentData()
+  comments: getCommentsData()
 });
 
-const getPicturesPhoto = () => Array.from({ length: PHOTOS_COUNT }, (v, k) => getPicturePhoto(k + 1));
+const getPhotosDescription = (count) => Array.from({ length: count }, (v, k) => getPhotoDescription(k + 1));
 
-getPicturesPhoto();
-
+getPhotosDescription(PHOTOS_COUNT);
