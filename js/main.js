9346e793aff3da3831ad0d7e2aff061605c91c93
Kangooboy <40115049+Kangooboy@users.noreply.github.com>
2022-03-10 23:11:20 +0200

    thumbnails

--- js/main.js
+++ js/main.js
@@ -1,3 +1,3 @@
-import { getPhotosDescription, PHOTOS_COUNT } from './data.js';
+import { createThumbnail } from './thumbnails.js';
 
-getPhotosDescription(PHOTOS_COUNT);
+createThumbnail();
--- js/thumbnails.js
+++ js/thumbnails.js
@@ -4,17 +4,19 @@ const arrayData = getPhotosDescription(PHOTOS_COUNT);
 const thumbnailContainer = document.querySelector('.pictures');
 const thumbnailListFragment = document.createDocumentFragment();
 const thumbnailTemplate = document.querySelector('#picture').content;
-const newThumbnail = thumbnailTemplate.querySelector('.picture');
+
 
 arrayData.forEach((dataValue) => {
-  const clonedThumbnail = newThumbnail.cloneNode(true);
-  const imgUrl = clonedThumbnail.querySelector('.picture__img');
-  const likesCount = clonedThumbnail.querySelector('.picture__likes');
-  const commentsCount = clonedThumbnail.querySelector('.picture__comments');
+  const newThumbnail = thumbnailTemplate.querySelector('.picture');
+  const imgUrl = newThumbnail.querySelector('.picture__img');
+  const likesCount = newThumbnail.querySelector('.picture__likes');
+  const commentsCount = newThumbnail.querySelector('.picture__comments');
   imgUrl.src = dataValue.url;
   likesCount.textContent = dataValue.likes;
   commentsCount.textContent = dataValue.comments.length;
-  thumbnailListFragment.appendChild(clonedThumbnail);
+  thumbnailListFragment.appendChild(newThumbnail.cloneNode(true));
 });
 
-thumbnailContainer.appendChild(thumbnailListFragment);
+const createThumbnail = () => thumbnailContainer.appendChild(thumbnailListFragment);
+
+export { createThumbnail };
