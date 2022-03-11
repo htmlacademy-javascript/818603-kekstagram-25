1a6648dcd5efeac49e3f4a60ea6f5219ab87a57f
Kangooboy <40115049+Kangooboy@users.noreply.github.com>
2022-03-11 13:32:31 +0200

    thumbnails

--- js/thumbnails.js
+++ js/thumbnails.js
@@ -4,17 +4,17 @@ const arrayData = getPhotosDescription(PHOTOS_COUNT);
 const thumbnailContainer = document.querySelector('.pictures');
 const thumbnailListFragment = document.createDocumentFragment();
 const thumbnailTemplate = document.querySelector('#picture').content;
-
+const newTemplate = thumbnailTemplate.querySelector('.picture');
 
 arrayData.forEach((dataValue) => {
-  const newThumbnail = thumbnailTemplate.querySelector('.picture');
+  const newThumbnail = newTemplate.cloneNode(true);
   const imgUrl = newThumbnail.querySelector('.picture__img');
   const likesCount = newThumbnail.querySelector('.picture__likes');
   const commentsCount = newThumbnail.querySelector('.picture__comments');
   imgUrl.src = dataValue.url;
   likesCount.textContent = dataValue.likes;
   commentsCount.textContent = dataValue.comments.length;
-  thumbnailListFragment.appendChild(newThumbnail.cloneNode(true));
+  thumbnailListFragment.appendChild(newThumbnail);
 });
 
 const createThumbnail = () => thumbnailContainer.appendChild(thumbnailListFragment);
